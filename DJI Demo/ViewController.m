a40c3d892422559dfe9f72b8366b3057fd5712d2
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2019-03-03 17:55:09 -0800

    setting up SDK in project

--- DJI Demo/ViewController.m
+++ DJI Demo/ViewController.m
@@ -7,17 +7,71 @@
 //
 
 #import "ViewController.h"
+#import <DJISDK/DJISDK.h>
 
-@interface ViewController ()
+@interface ViewController ()<DJISDKManagerDelegate>
 
 @end
 
 @implementation ViewController
+
+- (void)registerApp {
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+
+- (void)appRegisteredWithError:(NSError * _Nullable)error {
+    <#code#>
+}
+
+- (void)encodeWithCoder:(nonnull NSCoder *)aCoder {
+    <#code#>
+}
+
+- (void)traitCollectionDidChange:(nullable UITraitCollection *)previousTraitCollection {
+    <#code#>
+}
+
+- (void)preferredContentSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
+    <#code#>
+}
+
+//- (CGSize)sizeForChildContentContainer:(nonnull id<UIContentContainer>)container withParentContainerSize:(CGSize)parentSize {
+//    <#code#>
+//}
+
+- (void)systemLayoutFittingSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
+    <#code#>
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
+    <#code#>
+}
+
+- (void)willTransitionToTraitCollection:(nonnull UITraitCollection *)newCollection withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
+    <#code#>
+}
+
+- (void)didUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context withAnimationCoordinator:(nonnull UIFocusAnimationCoordinator *)coordinator {
+    <#code#>
+}
+
+- (void)setNeedsFocusUpdate {
+    <#code#>
+}
+
+- (BOOL)shouldUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context {
+    
+    return true;
+}
+
+- (void)updateFocusIfNeeded {
+    <#code#>
+}
 
 @end
